e002922c816da444d2726cb5db47bced8fad20b3
Niels Nielsen <nndev4@gmail.com>
2013-05-17 20:29:22 -0600

    maxSnaps adjusts max on snaps slider. Added input restrictions to the text boxes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,8 +2,33 @@
 
 angular.module('gcjvisApp')
   .controller('MainCtrl', ['$scope', function ($scope) {
+    var findMaxSnaps = function (snappers) {
+      return 4 * snappers + 2; // 2 * (2 * snappers + 1)
+    };
+
     $scope.snappers = 1;
     $scope.snaps = 0;
+    $scope.maxSnaps = findMaxSnaps($scope.snappers);
+
+    $scope.$watch('snappers', function (newVal, oldVal) {
+      if (newVal.length === 0) { return; }
+      var newValNum = parseInt(newVal, 10);
+      if (isNaN(newValNum) || String(newValNum).length !== String(newVal).length ||
+          newValNum < 1 || newValNum > 30) {
+        $scope.snappers = oldVal;
+      }
+
+      $scope.maxSnaps = findMaxSnaps(newVal);
+    });
+
+    $scope.$watch('snaps', function (newVal, oldVal) {
+      if (newVal.length === 0) { return; }
+      var newValNum = parseInt(newVal, 10);
+      if (isNaN(newValNum) || String(newValNum).length !== String(newVal).length ||
+          newValNum < 0 || newValNum > $scope.maxSnaps) {
+        $scope.snaps = oldVal;
+      }
+    });
 
     $scope.incSnaps = function () {
       $scope.snaps++;
@@ -14,22 +39,32 @@ angular.module('gcjvisApp')
   .directive('slider', [function () {
     return {
       scope: {
-        value: "="
+        value: "=",
+        max: "=",
+        label: "@"
       },
+      template: '<span class="sliderLabel">{{label}}</span>' +
+                '<span class="sliderMin">0</span>' +
+                '<span class="sliderMax">{{max}}</span>',
       link: function (scope, element, attrs) {
         var $el = $(element);
         var orientation = attrs.orientation || "horizontal";
         var reverseAdjustVal, startVal;
         if (attrs.reversed) {
           reverseAdjustVal = 1 + Number(attrs.max);
-          startVal = attrs.max;
+          startVal = scope.max;
         } else {
           startVal = attrs.min;
         }
 
+        scope.$watch("max", function (newVal, oldVal) {
+          scope.value = attrs.min;
+          $el.data('setup').settings.range[1] = newVal;
+        });
+
+
         scope.$watch("value", function (newVal, oldVal) {
           var adjustedNewVal;
-          console.log("new val:", newVal);
           if (attrs.reversed) {
             adjustedNewVal = reverseAdjustVal - newVal;
           } else {
@@ -41,7 +76,7 @@ angular.module('gcjvisApp')
         });
 
         $el.noUiSlider({
-            range: [attrs.min, attrs.max],
+            range: [attrs.min, scope.max],
             start: startVal,
             handles: 1,
             step: 1,
